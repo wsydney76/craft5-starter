67022296d5a3f8d438340958dab5ddea1d4161ae
wsydn <wsydney@web.de>
2024-03-27 10:46:37 +0100

    Craft 5.0.0

---
+++ config/preview-mate.php
@@ -0,0 +1,12 @@
+<?php
+//
+return [
+    "matrixFields" => [
+        [
+            "handle" => "bodyContent",
+            "excludedBlocks" => [
+                "rowContainer",
+            ],
+        ],
+    ],
+];
